e8c1d9965f2b887dc5891f75712aae316c44bb82
volcov <volcov.bruno@gmail.com>
2026-08-29 12:30:52 -0300

    redesign site with bento layout and new elixir-coaster logo

--- test/volcovland_web/controllers/page_controller_test.exs
+++ test/volcovland_web/controllers/page_controller_test.exs
@@ -3,7 +3,18 @@ defmodule VolcovlandWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Welcome to"
-    assert html_response(conn, 200) =~ "Volcovland"
+    assert html_response(conn, 200) =~ "Bruno"
+    assert html_response(conn, 200) =~ "volcovland"
+  end
+
+  test "GET / shows an empty state when there are no posts", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    html = html_response(conn, 200)
+
+    if Volcovland.Blog.all_posts() == [] do
+      assert html =~ "Nothing published yet"
+    else
+      assert html =~ Volcovland.Blog.recent_posts(1) |> hd() |> Map.fetch!(:title)
+    end
   end
 end
